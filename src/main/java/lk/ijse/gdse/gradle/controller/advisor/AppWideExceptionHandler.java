1201d884325a1539f033b2d750ad5161c0595480
deshanrajamanthrie <deshanrajamanthrie@gmail.com>
2023-09-29 10:42:25 +0530

    build project

--- src/main/java/lk/ijse/gdse/gradle/controller/advisor/AppWideExceptionHandler.java
+++ src/main/java/lk/ijse/gdse/gradle/controller/advisor/AppWideExceptionHandler.java
@@ -1,5 +1,8 @@
 package lk.ijse.gdse.gradle.controller.advisor;
 
+import lk.ijse.gdse.gradle.util.ResponseUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,8 +12,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class AppWideExceptionHandler {
 
     @ExceptionHandler({Exception.class})
-    public String exceptionHandler(Exception e){
-        return  e.getMessage();
+    public ResponseEntity exceptionHandler(Exception e) {
+        return new ResponseEntity(new ResponseUtil(500, e.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR);
 
     }
 }
